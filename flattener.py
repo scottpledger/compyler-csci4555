045d7ef30dccaa5f6d638bc4755903d7938d9135
Scott Pledger <scottpledger2005@gmail.com>
2012-08-31 09:54:49 -0600

    Added some of the in-class code for the flattener.

--- flattener.py
+++ flattener.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python2
 
+temp_var_c = 0
+
+def gen_temp():
+	tmp = 't'+temp_var_c
+	temp_var_c = temp_var_c + 1
+	return tmp
+
+def flatten(n):
+	if isinstance(n,Name):
+		return ( n, [] )
+	elif isinstance( n, Const ):
+		return ( n, [] )
+	elif isinstance( n, Add ):
+		(l,ss1) = flatten(n.left)
+		(r,ss2) = flatten(n.right)
+		t = gen_temp()
+		ss3 = [Assign(AssName(t),Add((l,r)))]
+		return ( Name( t ) , ss1 + ss2 + ss3 )
+
 
