173526bd590f2213d64c733b1e5e6221af027da7
Scott Pledger <scottpledger2005@gmail.com>
2012-09-05 10:07:00 -0600

    Added #! header to compiler.py, added some in-class code to assembler.py.

--- assembler.py
+++ assembler.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python2
 
-
+def instr_selection(s,d):
+	if(isinstance(s,Print)):
+		s0 = Push(cvt(s.nodes[0],d))
+		s1 = Call(print_int_nl)
+		s2 = AddInstr(4,Register('esp'))
+		return [s0,s1,s2]
