3d0cd5859f8b2fd0afec5958b915596d43de52bb
Scott Pledger <scottpledger2005@gmail.com>
2012-09-06 17:30:02 -0600

    Made the compiler ignore argv[0], so as not to run on itself.

--- compyler.py
+++ compyler.py
@@ -7,8 +7,10 @@ from compiler import *
 
 parser = argparse.ArgumentParser(description='Translates a .py file to x86 assembly language')
 parser.add_argument(  'infile', nargs='+', type=argparse.FileType('r'), default=sys.stdin  )
-ns = parser.parse_args(sys.argv)
+argv = sys.argv
+argv.pop(0)
+ns = parser.parse_args(argv)
 
-for infile in ns.infile:
-	tree = compiler.parse(infile.read())
+for input_file in ns.infile:
+	tree = compiler.parse(input_file.read())
 	print tree
