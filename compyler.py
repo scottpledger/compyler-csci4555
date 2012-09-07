0a82cb29f79d9e5bc2f082094e1f2b37a8ed24ea
Andrew Fischer <andrew.s.fischer-1@colorado.edu>
2012-09-07 00:22:23 -0600

    Start of work on flatnode_to_asm

--- compyler.py
+++ compyler.py
@@ -64,7 +64,9 @@ def flatnode_to_asm(n, output):
 	if isinstance( n, Assign ):
 		if isinstance( n.expr, CallFunc ):
 			if n.expr.node.name == 'input'
-				x86Str = '\tcall input\n' + '\tmovl %eax, ' + varoff_dict[n.nodes.name] +'(%ebp)'
+				x86Str = '''
+	call input
+	movl %eax, ''' + varoff_dict[n.nodes.name] +'''(%ebp)'''
 			elif 
 		elif isinstance( n.expr, UnarySub ):
 			x86Str = 'movl ' 
