cb9716a84e2267c00395c9ea4ac62f42c22c03bb
Scott Pledger <scottpledger2005@gmail.com>
2012-09-06 20:51:00 -0600

    Added the Sub function to the flattener.

--- flattener.py
+++ flattener.py
@@ -5,7 +5,8 @@ from compiler import *
 temp_var_c = 0
 
 def gen_temp():
-	tmp = 't'+temp_var_c
+	global temp_var_c
+	tmp = 't'+str(temp_var_c)
 	temp_var_c = temp_var_c + 1
 	return tmp
 
@@ -13,15 +14,21 @@ def flatten(n):
 	if isinstance(n,Module):
 		return flatten(n.node)
 	elif isinstance(n,Stmt):
-		return ( n, sum( [ flatten(m) for m in n.nodes ] ) )
+		return ( n, [ flatten(m) for m in n.nodes ] )
 	elif isinstance(n,Printnl):
-		return ( n, flatten(n.nodes[0]) )
+		args = []
+		flat = []
+		for m in n.nodes:
+			(name,flt) = flatten(m)
+			args = args + [name]
+			flat = flat + [(name,flt)] + flt
+		return ( CallFunc(Name('print_int'), args), flat )
 	elif isinstance(n,Assign):
-		return ( n, sum( [ flatten(m) for m in n.nodes ] + [flatten(n.expr)] ) )
+		return ( n, [ flatten(m) for m in n.nodes ] + [flatten(n.expr)] )
 	elif isinstance(n,AssName):
-		return ( n, sum( [ flatten(m) for m in n.nodes ] ) )
+		return ( n, [] )
 	elif isinstance(n, Discard):
-		return ( n, flatten(n.expr) )
+		return flatten(n.expr)
 	elif isinstance( n, Const ):
 		return ( n, [] )
 	elif isinstance(n,Name):
@@ -30,11 +37,18 @@ def flatten(n):
 		(l,ss1) = flatten(n.left)
 		(r,ss2) = flatten(n.right)
 		t = gen_temp()
-		ss3 = [Assign(AssName(t),Add((l,r)))]
+		ss3 = [Assign(AssName(t,''),Add((l,r)))]
+		return ( Name( t ) , ss1 + ss2 + ss3 )
+	elif isinstance( n, Sub ):
+		(l,ss1) = flatten(n.left)
+		(r,ss2) = flatten(n.right)
+		t = gen_temp()
+		ss3 = [Assign(AssName(t,''),Sub((l,r)))]
 		return ( Name( t ) , ss1 + ss2 + ss3 )
 	elif isinstance( n, UnarySub ):
-		return ( n, flatten(n.expr) )
+		t = gen_temp()
+		return ( Name( t ), [flatten(n.expr)] )
 	elif isinstance( n, CallFunc ):
-		return ( n, sum( [ flatten(m) for m in n.args ] ) + [ flatten(n.node) ] )
-
+		t = gen_temp()
+		return ( Name( t ), [ n ] )
 
