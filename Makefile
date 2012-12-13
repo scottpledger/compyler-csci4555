b6023a6d98f732723f30b2564d58bb9857edeab6
Scott Pledger <scottpledger2005@gmail.com>
2012-12-12 18:17:18 -0700

    Added the gc.c/h files and fixed the Makefile to successfully build the .s files.

--- Makefile
+++ Makefile
@@ -6,15 +6,40 @@ TEX_EXTRAFL=pydocs.toc pydocs.tex.backup pydocs.aux pydocs.log pydocs.pdf
 
 P0_FILES_PY=$(shell find ./tests/p0/ -name '*.py')
 P0_FILES_AS=${P0_FILES_PY:%.py=%.s}
+P0_FILES_EX=${P0_FILES_PY:%.py=%.out}
 P1_FILES_PY=$(shell find ./tests/p1/ -name '*.py')
 P1_FILES_AS=${P1_FILES_PY:%.py=%.s}
+P1_FILES_EX=${P1_FILES_PY:%.py=%.out}
 P2_FILES_PY=$(shell find ./tests/p2/ -name '*.py')
 P2_FILES_AS=${P2_FILES_PY:%.py=%.s}
+P2_FILES_EX=${P2_FILES_PY:%.py=%.out}
 P3_FILES_PY=$(shell find ./tests/p3/ -name '*.py')
 P3_FILES_AS=${P3_FILES_PY:%.py=%.s}
+P3_FILES_EX=${P3_FILES_PY:%.py=%.out}
+RUNTIME_FILES=$(shell find ./ -maxdepth 1 -name '*.c')
+RUNTIME_HDRS =$(shell find ./ -maxdepth 1 -name '*.h')
+RUNTIME_FILES_LIB=${RUNTIME_FILES:%.c=%.o}
+RUNTIME_FILES_LINK=${RUNTIME_FILES_LIB:./%.o=-l%}
+PWD=$(shell pwd)
+
+
+GCC_INCLUDES= -I. ${RUNTIME_HDRS}
+GCC_LINKS = -L. -lcompyler -lm
+
+%.o: %.c
+	gcc -c -fPIC -m32 $*.c -o $*.o
+	
+runtime-files: ${RUNTIME_FILES_LIB}
+	gcc -shared -m32 -lm -Wl,--as-needed,-soname,libcompyler.so -o libcompyler.so  ${RUNTIME_FILES_LIB}
+	
+	
 
 %.s: %.py
-	python compiler.py $*.py
+	python compile.py $*.py
+
+%.out: %.s runtime-files
+	gcc -m32 $*.s -o $*.bin -O -g  $(GCC_LINKS)
+	
 
 clean-pydocs:
 	rm -f $(TEX_EXTRAFL)
@@ -24,7 +49,6 @@ pydocs:
 	pdflatex $(TEX_TEXFILE)
 	pdflatex $(TEX_TEXFILE)
 	pdflatex $(TEX_TEXFILE)
-	
 
 clean-submit:
 	rm -f *.pyc *~ *.zip 
@@ -33,16 +57,47 @@ submit: clean-submit
 	zip $(ZIP_OUTFILE) *
 	zip -d $(ZIP_OUTFILE) $(ZIP_EXCLUDE) $(TEX_TEXFILE) $(TEX_EXTRAFL)
 
-clean-all: clean-pydocs clean-submit
+clean-all: clean-pydocs clean-submit clean-asm-p0 clean-asm-p1 clean-asm-p2 clean-asm-p3 clean-runtime-files
 all: submit pydocs
 clean: clean-submit
 
 
+clean-runtime-files:
+	rm -f *.o
+	rm -f *.so*
 	
 gen-asm-p0: ${P0_FILES_AS}
+
+gen-exe-p0: ${P0_FILES_EX}
+
+clean-asm-p0:
+	rm -f ./tests/p0/*.s
+	
 gen-asm-p1: ${P1_FILES_AS}
+
+gen-exe-p1: ${P1_FILES_EX}
+
+clean-asm-p1:
+	rm -f ./tests/p1/*.s
+	
 gen-asm-p2: ${P2_FILES_AS}
+
+gen-exe-p2: ${P2_FILES_EX}
+
+clean-asm-p2:
+	rm -f ./tests/p2/*.s
+	
 gen-asm-p3: ${P3_FILES_AS}
 
+gen-exe-p3: ${P3_FILES_EX}
+
+clean-asm-p3:
+	rm -f ./tests/p3/*.s
+	
+
 gen-asm-tests: gen-asm-p0 gen-asm-p1 gen-asm-p2 gen-asm-p3
+
+gen-exe-tests: gen-exe-p0 gen-exe-p1 gen-exe-p2 gen-exe-p3
+
+
 	
---
+++ gc.h
@@ -0,0 +1,103 @@
+#ifndef _gc_h_
+#define _gc_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+//
+// These definitions describe the "root set" or the pointers
+// into the allocated heap.
+//
+// We define a limited number of pointers into the root set
+// and in general, a system would identify global and local
+// variables that are pointers.
+//
+
+#define ROOT_SET_LENGTH 1024
+#define ROOT_SET_NULL   NULL
+extern void *root_set[ROOT_SET_LENGTH];
+
+//
+// Each object that is allocated is marked with a specific "type"
+// The type indicates the size of the object and offset of up
+// to 16 pointers in the object. The location of the pointers
+// is expressed using an offset from the (user) part of the
+// object in terms of integratl 4-byte offsets. For example,
+// 0 means 0 bytes, 1 means 4 bytes, 2 means 8 bytes and so on.
+//
+
+#define GC_TYPE_INFO_MAX_POINTERS 16
+typedef struct {
+  int size_in_bytes;  
+  int pointers[GC_TYPE_INFO_MAX_POINTERS];
+  int tenured;
+  //
+  // you can add additional fields to the gc_type_info as you
+  // see fit. Some example might include pointers for a
+  // freelist of items (should that be useful).
+  //
+} gc_type_info;
+
+
+//
+// The gc_init() routine should be called before anything is allocated.
+// This is used to initialize your code & data structures.
+//
+extern void gc_init();
+
+
+//
+// Allocate an object of the appropriate type and return a pointer to
+// the user portion of the space. The returned address should be
+// aligned for a double (i.e. 8 byte aligned).
+//
+extern void *gc_alloc(gc_type_info *);
+
+
+//
+// This routine inserts a check that the returned item is 8-byte aligned
+// (the bottom 3 bits are zero);
+// 
+static inline void *gc_alloc_check(gc_type_info *info)
+{
+  void *ret = gc_alloc(info);
+  int iret = (int) ret;
+  if ( (iret & 0x7) != 0 ) {
+    fprintf(stderr,"Allocation of object not 8-byte aligned\n");
+    exit(1);
+  }
+  return ret;
+}
+
+
+//
+// Start a collection phase. This can be called by the user program
+// but your garbage collector is free to do whatever it wants when
+// this is called.
+//
+extern void gc_collect();
+
+
+//
+// Returns an eight byte alligned size value based on the size value
+// in passed gc_type_info *
+//
+extern int size_in_bytes(gc_type_info *);
+
+
+//
+// returns 1 if there is enough room for payload + type meta-data
+// returns 0 if not
+//
+extern int have_room(gc_type_info *);
+
+
+//
+// takes pointer to an object, copys that object to heap, then returns
+// the new pointer to that object
+//
+extern int * gc_copy(int *);
+
+
+#endif
